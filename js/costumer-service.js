f41b99d09a5ee5e34b00f1ce04bf983433209e47
Ryan Oliveira <oryan424@gmail.com>
2021-04-29 21:31:01 -0300

    Update

--- js/costumer-service.js
+++ js/costumer-service.js
@@ -9,14 +9,15 @@ function onSubmit(event) {
     const message = document.querySelector('#message')
 
     const result = document.querySelector('#result')
-
+    
     result.innerHTML = ''
     name.className = ''
     surname.className = ''
     email.className = ''
     phone.className = ''
-    department.className = ''
-    message.className = ''
+    
+    document.querySelector('select').style.borderColor = '#071A52'
+    document.querySelector('textarea').style.borderColor = '#071A52'
 
     if (!name.value || !isNaN(name.value)) {
         result.innerHTML = '<p class="message-error">Informe seu nome!</p>'
@@ -31,16 +32,18 @@ function onSubmit(event) {
         email.className = 'field-error'
         email.focus()
     } else if (!phone.value || isNaN(phone.value) || phone.value.length !== 11 ) {
-        result.innerHTML = '<p class="message-error">Informe seu telefone!</p>'
+        result.innerHTML = '<p class="message-error">Informe seu contato!</p>'
         phone.className = 'field-error'
         phone.focus()
     } else if (!department.value || !isNaN(department.value)) {
         result.innerHTML = '<p class="message-error">Escolha um departamento!</p>'
-        department.className = 'field-error'
+        document.querySelector('select').style.borderColor = '#D10'
     } else if (!message.value || !isNaN(message.value)) {
         result.innerHTML = '<p class="message-error">Informe sua mensagem!</p>'
-        message.className = 'field-error'
+        document.querySelector('textarea').style.borderColor = '#D10'
         message.focus()
+    } else {
+        result.innerHTML = '<p class="confirm">Mensagem enviada!</p>'
     }
 }
 
